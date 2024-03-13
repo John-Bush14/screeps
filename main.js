1a4e55f6311cc9857f7f777736739e59cf15924a
John-Bush14 <joppe@stuyck.be>
2024-03-13 19:24:44 +0100

    fix(only create creeps if max energy)

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var creepCreation = require("creep.creation")
+var spawn = Game.spawns["Main"]
 
-module.exports.loop = function () { creepCreation.create();
+module.exports.loop = function () { 
+
+   if (spawn.store.getFreeCapacity() == 0) {creepCreation.create();}
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
