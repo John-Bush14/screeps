89d6b25a5170ee58120aed61a8a5a7ad8ef89ecf
John-Bush14 <139780397+John-Bush14@users.noreply.github.com>
2024-03-13 16:50:53 +0100

    test

---
+++ creep.renewal.js
@@ -0,0 +1,24 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('creep.renewal');
+ * mod.thing == 'a thing'; // true
+ */
+
+module.exports = { renew() {
+    var minimums = [3, 1, 1];
+    var roles = ["harvester", "upgrader", "builder"];
+    var fulfilled = 0;
+    
+    for(var i in [0, 1, 2]) {
+        roleAmount = _.filter(Game.creeps, (creep) => creep.memory.role == roles[i]).length;
+        if (roleAmount < minimums[i]) {
+            Game.spawns["Main"].spawnCreep([WORK, MOVE, CARRY], role[i]+(roleAmount+1).toString(), {memory: {"role": roles[i]}})
+        } else {fulfilled += 1}
+    } if (fulfilled == 3) {
+        Game.spawns["Main"].spawnCreep(
+            [WORK, MOVE, CARRY], "H"+_.filter(Game.creeps, (creep) => creep.memory.role == roles[i]).length.toString(), {memory: {"role": "builder"}})
+    }
+}};
