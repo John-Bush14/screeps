cb520de7a1d4e79120573858887e08a1719cbd29
John-Bush14 <joppe@stuyck.be>
2024-03-13 19:29:51 +0100

    refactore.scalability(main.js roles)

--- main.js
+++ main.js
@@ -1,27 +1,23 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-var creepCreation = require("creep.creation")
-var spawn = Game.spawns["Main"]
+var roleCharger = require("role.charger");
+var creepCreation = require("creep.creation");
+var spawn = Game.spawns["Main"];
+var roles = {
+   harvester: roleHarvester,
+   upgrader: roleUpgrader,
+   builder: ruleBuilder,
+   charger: roleCharger
+};
 
 module.exports.loop = function () { 
 
    if (spawn.store.getFreeCapacity() == 0) {creepCreation.create();}
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memord.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            if (creep.room.find(FIND_CONSTRUCTION_SITES).length) {
-                roleBuilder.run(creep)} 
-            else {
-                roleHarvester.run(creep)        
-            }
-        }
-    }
+      for(var name in Game.creeps) {
+         var creep = Game.creeps[name];
+
+         roles[creep.memory.role].run();
+      }
 }
